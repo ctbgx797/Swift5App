ded9fcbf8a1ce45d32464dc6cee5a9041fbfa9e7
Yoshiki <villager2525@gmail.com>
2020-12-06 16:43:26 +0900

    ボタンをタグ管理で分けた

--- QuestionApp/QuestionApp/Controller/ViewController.swift
+++ QuestionApp/QuestionApp/Controller/ViewController.swift
@@ -9,11 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var maxScoreLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
+    @IBAction func answer(_ sender: Any) {
 
+        if (sender as AnyObject).tag == 1{
+        
+            //○ボタンが押されたとき
+            
+            //ユーザーが押したボタンが○ボタンだった
+            
+            //○ボタンの音を流す
+            
+        }else if (sender as AnyObject).tag == 2{
+            
+            //✕ボタンが押されたとき
+            
+            //ユーザーが押したボタンが✕ボタンだった
+            
+            //✕ボタンの音を流す
+            
+        }
+        
+    }
+    
 
 }
 
