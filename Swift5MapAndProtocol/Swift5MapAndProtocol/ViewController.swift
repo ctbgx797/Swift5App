4b8fea97fa1ab790b42310d8734914461c1d35f1
Yoshiki <villager2525@gmail.com>
2020-12-06 13:21:41 +0900

    ロングタップ処理を実装

--- Swift5MapAndProtocol/Swift5MapAndProtocol/ViewController.swift
+++ Swift5MapAndProtocol/Swift5MapAndProtocol/ViewController.swift
@@ -6,13 +6,50 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,CLLocationManagerDelegate,UIGestureRecognizerDelegate {
 
+    @IBOutlet var longPress: UILongPressGestureRecognizer!
+    @IBOutlet var mapView: MKMapView!
+    var locManager:CLLocationManager!
+    
+    @IBOutlet var settingButton: UIButton!
+    
+    @IBOutlet var addressLabel: UILabel!
+    var addressString = String()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        settingButton.backgroundColor = .white
+        settingButton.layer.cornerRadius = 20.0
+ 
     }
+    
+    @IBAction func longPressTap(_ sender: UILongPressGestureRecognizer) {
+        
+        
+        if sender.state  == .began{
+            //tapを開始したとき
+            
+            
+        }else if sender.state == .ended{
+            //tapを終了したとき
+            
+            //tapした位置を指定して､MapViewの緯度経度を取得する
+            
+            //緯度経度から住所に変換する
+
+        }
+        
+        
+        
+    
+    
+    }
+    
 
 
 }
