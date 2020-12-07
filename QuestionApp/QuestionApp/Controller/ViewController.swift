5ef0582c41072e8cdcc9749006c65ff2bb29a4f7
Yoshiki <villager2525@gmail.com>
2020-12-07 23:07:06 +0900

    ImagesListクラスをViewControllerでインスタンス化した

--- QuestionApp/QuestionApp/Controller/ViewController.swift
+++ QuestionApp/QuestionApp/Controller/ViewController.swift
@@ -12,6 +12,11 @@ class ViewController: UIViewController {
     @IBOutlet var imageView: UIImageView!
     @IBOutlet var maxScoreLabel: UILabel!
     
+    let imagesList = ImagesList()
+    
+    //IBActionで検知した正答がどちらなのかを取得する
+    var pickedAnswer = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -23,20 +28,27 @@ class ViewController: UIViewController {
         if (sender as AnyObject).tag == 1{
         
             //○ボタンが押されたとき
+            pickedAnswer = true
             
             //ユーザーが押したボタンが○ボタンだった
             
+            
             //○ボタンの音を流す
+            
             
         }else if (sender as AnyObject).tag == 2{
             
             //✕ボタンが押されたとき
+            pickedAnswer = false
             
             //ユーザーが押したボタンが✕ボタンだった
+            
             
             //✕ボタンの音を流す
             
         }
+        
+        //チェック:回答があっているか(pickedAnswerとImagesListのcorrectOrNotの値が一致しているかどうか)
         
     }
     
