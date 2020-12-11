80b4ee47175fbefc614a28bab3797ac9687289dd
Yoshiki <villager2525@gmail.com>
2020-12-11 17:11:57 +0900

    音声モデルの作成

---
+++ QuestionApp/QuestionApp/Model/SoundFile.swift
@@ -0,0 +1,30 @@
+//
+//  SoundFile.swift
+//  QuestionApp
+//
+//  Created by 西谷恭紀 on 2020/12/11.
+//
+
+import Foundation
+import AVFoundation
+
+
+class SoundFile{
+    var player:AVAudioPlayer?
+    
+    func playSound(filename:String,extensionName:String){
+        
+        //再生する
+        let soundURL = Bundle.main.url(forResource: filename, withExtension: extensionName)
+        
+        do {
+             //効果音を鳴らす
+            player = try AVAudioPlayer(contentsOf:soundURL!)
+            player?.play()
+            
+        } catch  {
+            print("エラーです")
+        }
+        
+    }
+}
