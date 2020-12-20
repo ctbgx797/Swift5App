bbf7e6a542eb60b308d75b8f54431bb39a926941
Yoshiki <villager2525@gmail.com>
2020-12-21 00:07:43 +0900

    for文を用いて自動でラベルを設定

--- Swift5introApp/Swift5introApp/Controller/IntroViewController.swift
+++ Swift5introApp/Swift5introApp/Controller/IntroViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import Lottie
 
-class IntroViewController: UIViewController {
+class IntroViewController: UIViewController,UIScrollViewDelegate {
 
     @IBOutlet var scrollView: UIScrollView!
     
@@ -20,6 +20,7 @@ class IntroViewController: UIViewController {
         
         //ページングができるようになる
         scrollView.isPagingEnabled = true
+        setUpScroll()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -30,6 +31,20 @@ class IntroViewController: UIViewController {
     
     func setUpScroll() {
         
+        scrollView.delegate = self
+        
+        scrollView.contentSize = CGSize(width: view.frame.size.width * 5, height: scrollView.frame.size.height)
+        
+        for i in 0...4{
+            
+            let onboardLabel = UILabel(frame: CGRect(x: CGFloat(i) * view.frame.size.width, y: view.frame.size.height/3, width: scrollView.frame.size.width, height: scrollView.frame.size.height))
+            
+            onboardLabel.font = UIFont.boldSystemFont(ofSize:15.0)
+            onboardLabel.textAlignment = .center
+            onboardLabel.text = onboardStringArray[i]
+            scrollView.addSubview(onboardLabel)
+            
+        }
     }
 
     
